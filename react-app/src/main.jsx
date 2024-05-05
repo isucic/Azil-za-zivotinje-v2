72cf413404b75882ec789a9a72e9ba191edcd6ab
isucic <isucic00@fesb.hr>
2024-05-05 22:31:40 +0200

    delete files

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 // import './index.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = "http://localhost:3000"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
